9f65feaaee058bd888daec414824c5a4aec2eede
0mairAftab <omairch035@gmail.com>
2026-07-16 11:50:58 +0500

    did the loading UI in nextjs .. its basically used when content is loading as we showed the animations during the previous project

---
+++ my-app/app/blog/loading.tsx
@@ -0,0 +1,6 @@
+
+export default async function loading() {
+
+    
+    return <h2 className=" text-3xl text-center flex justify-center mt-90 items-center">Loading blog page......</h2>
+}
--- my-app/app/blog/page.tsx
+++ my-app/app/blog/page.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
 
-export default function blog(){
+// // And to actually test delay as mentioned  in loading.tsx it, put the delay in page.tsx:
+
+export default async function blog(){
+    
+    await new Promise((resolve) => setTimeout(resolve, 3000)); // testing the loading.tsx file by adding a delay of 4 seconds before rendering the blog page
+
     return <h1>Blog page</h1>
 }
