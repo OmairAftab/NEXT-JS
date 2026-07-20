6ab3da397e8f2034342df73e3a070253ce63609a
0mairAftab <omairch035@gmail.com>
2026-07-20 15:26:08 +0500

    practiced 39 - Handling PATCH Requests

--- route-handlers-fromvideo35-/app/comments/[id]/route.ts
+++ route-handlers-fromvideo35-/app/comments/[id]/route.ts
@@ -12,3 +12,22 @@ export async function GET(request: Request, {params}:  {params: Promise<{id: str
 
     return Response.json(comment);
 }
+
+
+
+
+export async function PATCH(request: Request, {params}: {params: Promise<{id: string}>}){
+
+    const {id}=await params;
+    const body=await request.json(); //{text: "...."}  //this is the body of the request which we are sending from the client side in the fetch request.
+    
+    const commentIndex=comments.findIndex((c)=>c.id===parseInt(id)); //findIndex gives the index of the comment in the comments array which has the id same as the id in the params. If no comment is found with the given id, it returns -1.
+
+    if(commentIndex===-1){
+        return new Response("Comment not found", {status: 404});
+    }
+
+    comments[commentIndex].text=body.text;
+
+    return Response.json(comments[commentIndex]);
+}
