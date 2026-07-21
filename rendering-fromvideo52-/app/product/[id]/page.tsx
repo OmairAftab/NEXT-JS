590bb6f6238aaaf1cff655ef4729b8a7fba37a95
0mairAftab <omairch035@gmail.com>
2026-07-21 21:37:19 +0500

    did 57 - dynamicParams

--- rendering-fromvideo52-/app/product/[id]/page.tsx
+++ rendering-fromvideo52-/app/product/[id]/page.tsx
@@ -1,6 +1,14 @@
+//dynamicparams is initially set to true and when 
+//dynamicparams is set to true then it will statically render pages on demand for any dynamic route not included in the generateStaticParams function and it will be rendered on demand at runtime 
+
+//when dynamicparams is set to false then it will only render pages for the dynamic routes included in the generateStaticParams function and any other dynamic route not included in the generateStaticParams function will show 404 page
+
+export const dynamicParams = false;
+
+
+
 //generatestsaticparams function is used to generate static params for dynamic routes in Next.js.
 // It allows you to pre-render pages with specific parameters at build time instead of on deamnd at rutime like usual dynamic routes, hence improving performance and SEO.
-
 
 export async function generateStaticParams() {
     return[{id: "1"}, {id: "2"}, {id: "3"}]
