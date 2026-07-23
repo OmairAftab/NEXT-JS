a3952a04969b10c704fc9fbbaf3f94e6d0ab2805
0mairAftab <omairch035@gmail.com>
2026-07-23 15:44:59 +0500

    practiced 88 - Protecting Routes ..... how createRouteMatcher helps in clerk to prevent not signed in user to access the routes we mention

--- authentication-fromvideo83-/proxy.ts
+++ authentication-fromvideo83-/proxy.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware,  createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+const isProtectedRoute=createRouteMatcher(["/user-profile"])        //user-profile route is protected route, user must be signed in to access this route
+
+export default clerkMiddleware(async (auth,req)=>{
+    if(isProtectedRoute(req))  {        //if request is for protected route, then check if user is signed in or not
+       await auth.protect();          //if user is not signed in protect method will redirect user to sign in page 
+    }
+})
 
 export const config = {
   matcher: [
