d4685fe80617f4563b5334a6f1fa9d6da19d2a1d
0mairAftab <omairch035@gmail.com>
2026-07-24 11:53:55 +0500

    did 90 - Role Based Access Control

--- authentication-fromvideo83-/proxy.ts
+++ authentication-fromvideo83-/proxy.ts
@@ -1,11 +1,26 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
 const isProtectedRoute = createRouteMatcher(["/user-profile"])        //user-profile route is protected route, user must be signed in to access this route
+
+const isAdminRoute=createRouteMatcher(["/admin"])        
 
 export default clerkMiddleware(async (auth,req)=>{
     if(isProtectedRoute(req))  {        //if request is for protected route, then check if user is signed in or not
        await auth.protect();          //if user is not signed in protect method will redirect user to sign in page 
     }
+
+
+    const claims = (await auth()).sessionClaims;
+    console.log("Session claims:", claims); // check your TERMINAL, not browser
+
+
+
+      if ( isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== "admin") {
+          const url = new URL("/", req.url);
+          return NextResponse.redirect(url);
+      }
+
 })
 
 export const config = {
